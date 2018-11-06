950e26c75bc7d1d63dfe77a34c6cb17bda5dbc98
Wes Galbraith <galbwe92@gmail.com>
2018-11-06 09:52:52 -0700

    added test data for bowls. added eslint dependency

--- seed.js
+++ seed.js
@@ -123,7 +123,64 @@ pieces = [
     photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p592_i1_w640.jpeg'],
     priceDollars: 42,
     description: 'Wheel thrown, wax design, layered glazes'
-  }
+  },
+  {
+    name: 'Chattered Sunflower Bowl',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p699_i2_w2560.jpeg',
+    'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p699_i1_w2560.jpeg'],
+    priceDollars: 84,
+    description: 'Wheel thrown and chattered with carved sunflowers and slip trailed designs.'
+  },
+  {
+    name: 'Watermelon Oil Spot Bowls',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p710_i4_w640.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p710_i3_w640.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p710_i2_w640.jpeg'],
+    priceDollars: 42,
+    description: 'Wheel thrown, wax design, layered glazes'
+  },
+  {
+    name: 'Chattered Leafy sunflower serving bowl',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p700_i1_w2560.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p700_i2_w2560.jpeg'],
+    priceDollars: 86,
+    description: 'Wheel thrown, wax design, layered glazes'
+  },
+  {
+    name: 'Garden Gate leafy Sunflower bowl',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p697_i1_w2560.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p697_i3_w2560.jpeg'],
+    priceDollars: 68,
+    description: 'Wheel Thrown with carved leafy sunflowers and decorative slip trailing. Layered Glazes.'
+  },
+  {
+    name: 'Oil Spot all purpose bowls',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p682_i1_w640.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p682_i2_w640.jpeg'],
+    priceDollars: 46,
+    description: 'Wheel thrown, wax brush work, layered oil spot glazes.'
+  },
+  {
+    name: 'Slip trailed soup bowls',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p676_i2_w640.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p676_i3_w2560.jpeg'],
+    priceDollars: 42,
+    description: 'Wheel thrown, wax design, layered glazes'
+  },
+  {
+    name: 'Slip Trailed Sapphire Serving Bowl',
+    category: 'bowls',
+    photos: ['http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p509_i3_w640.jpeg',
+             'http://www.eastridgepottery.com/uploads/3/4/3/9/34391077/s869133620441393559_p509_i2_w2560.jpeg'],
+    priceDollars: 78,
+    description: 'Wheel thrown, Black slip trailed design'
+  },
 ]
 
 Piece.insertMany(pieces, function(err, p){
